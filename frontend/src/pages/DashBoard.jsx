0ccbdbe39bec4468719d98c3b57dbd13e272fd2d
lakshay0501 <79932200+lakshay0501@users.noreply.github.com>
2024-03-05 17:48:41 +0530

    second commit

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import App from "../App";
 import { Appbar } from "../components/Appbar";
 import {Balance} from "../components/Balance"
 import { Users } from "../components/Users";
 
 export const Dashboard = () => {
+    const [amount,setAmount] = useState(0);
+
     return <>
        <Appbar></Appbar>
        <div className="m-8">
-          <Balance value={"10,000"}></Balance>
+           
+          <Balance value={amount}></Balance>
           <Users></Users>
        </div>
     </>
