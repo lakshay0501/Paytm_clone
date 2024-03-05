0a564ed244b2cadfe9e3127ecde0079455649b0d
lakshay0501 <79932200+lakshay0501@users.noreply.github.com>
2024-03-05 14:36:13 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import {
   Routes,
 } from "react-router-dom";
 import { Signup } from './pages/Signup'
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/DashBoard";
+import { SendMoney } from "./pages/SendMoney";
 import './index.css'
 
 function App() {
@@ -16,11 +19,11 @@ function App() {
         <Routes>
            <Route path="/signup" element={<Signup/>}></Route>
            
-           {/* <Route path="/signin" element={<Signin/>}></Route> */}
+           <Route path="/signin" element={<Signin/>}></Route>
            
-           {/* <Route path="/dashboard" element={<Dashboard/>}></Route> */}
+           <Route path="/dashboard" element={<Dashboard/>}></Route>
            
-           {/* <Route path="/send" element={<SendMoney/>}></Route> */}
+           <Route path="/send" element={<SendMoney/>}></Route>
       
         </Routes>
 
---
+++ frontend/src/pages/DashBoard.jsx
@@ -0,0 +1,14 @@
+import App from "../App";
+import { Appbar } from "../components/Appbar";
+import {Balance} from "../components/Balance"
+import { Users } from "../components/Users";
+
+export const Dashboard = () => {
+    return <>
+       <Appbar></Appbar>
+       <div className="m-8">
+          <Balance value={"10,000"}></Balance>
+          <Users></Users>
+       </div>
+    </>
+}
