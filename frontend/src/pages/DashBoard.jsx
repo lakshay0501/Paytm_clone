2aec794584d2faecc4a1176440f05159bf194f4a
lakshay0501 <79932200+lakshay0501@users.noreply.github.com>
2024-03-06 17:28:57 +0530

    third commit

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import App from "../App";
 import { Appbar } from "../components/Appbar";
 import {Balance} from "../components/Balance"
 import { Users } from "../components/Users";
+import axios from "axios";
 
 export const Dashboard = () => {
     const [amount,setAmount] = useState(0);
+    
+    useEffect(()=>{
+       
+        async function fetchData(){
+             const response = await axios.get("http://localhost:3900/api/v1/account/balance",{
+                headers:{
+                    Authorization:`Bearer ${localStorage.getItem("token")}`
+                }
+            });
+            const {balance} = response.data;
+            setAmount(balance)
+        }
+        fetchData()
+    },[amount])
 
     return <>
        <Appbar></Appbar>
